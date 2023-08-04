479e85751ec9b52c818f871e92b3d322718d54dc
sangkyu <doesol1301@gmail.com>
2023-08-04 19:29:39 +0900

    수강신청 화이팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Code from "./code.js";
+import "./App.css";
+import menubar from "./menubar.png";
+import menu from "./menu.png";
 
 function App() {
+  const [modalShow, setModalShow] = useState(false);
+  const [classList, setClassList] = useState([
+    "알고리즘",
+    "확률통계및프로그래밍",
+    "신호및시스템",
+    "교양1",
+    "교양2",
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container row">
+      <div className="menubar col-2 box">
+        <img className="menubar-img" src={menubar} />
+      </div>
+
+      <div className="col box">
+        <div className="menu">
+          <img className="menu-img" src={menu} />
+        </div>
+        <div className="">
+          {classList.map(function (c, i) {
+            return (
+              <div key={c.id} className="row class-list">
+                <Code
+                  className="modal"
+                  classList={classList}
+                  setClassList={setClassList}
+                  index={i}
+                  setModalShow={setModalShow}
+                  show={modalShow}
+                  onHide={() => setModalShow(false)}
+                  backdrop={false}
+                ></Code>
+                <div className="col-1 btn-div">
+                  <p onClick={() => setModalShow(true)} className="apply-btn">
+                    신청
+                  </p>
+                </div>
+                <div className="col class-name">
+                  <p>{c}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/code.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
+import "./code.css";
+const Code = (props) => {
+  let [answer, setAnswer] = useState(getRandomNumber());
+  const [guess, setGuess] = useState("");
+  const [result, setResult] = useState("");
+
+  function getRandomNumber() {
+    let result = "";
+    const candidates = "0123456789";
+
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * candidates.length);
+      result += candidates[randomIndex];
+    }
+
+    return result;
+  }
+
+  function checkAnswer() {
+    if (answer === guess) {
+      let newList = [...props.classList];
+      newList.splice(props.index, 1);
+      props.setClassList(newList);
+      console.log("일치");
+      props.setModalShow(false);
+    } else {
+      console.log("불일치");
+    }
+    setGuess("");
+    setAnswer(getRandomNumber());
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title
+          id="contained-modal-title-vcenter"
+          className="submit-title"
+        >
+          매크로방지 코드입력
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="row">
+          <div className="col">
+            <p className="text">생성된 코드</p>
+            <p className="code col">{answer}</p>
+          </div>
+          <div className="col">
+            <p className="text">생성된 코드 입력</p>
+            <input
+              className="num-input col"
+              type="text"
+              value={guess}
+              onChange={(e) => setGuess(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="submit-div">
+          <button className="submit" onClick={checkAnswer}>
+            코드 입력
+          </button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default Code;
